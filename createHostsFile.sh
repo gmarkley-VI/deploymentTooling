3311bb4859c5a51f4b6d7d422e1f5995941e5186
gmarkley-VI <gmarkley@redhat.com>
2020-04-09 14:24:40 -0400

    Cleaned up inputs to allow passwords from bugging out.

--- createHostsFile.sh
+++ createHostsFile.sh
@@ -4,9 +4,9 @@ CURL=$(oc cluster-info | head -n1 | sed 's/.*\/\/api.//g'| sed 's/:.*//g')
 echo -n "Enter Windows User Name and press [ENTER]: "
 read NAME
 echo -n "Enter Windows User Password and press [ENTER]: "
-read -n 1 PASSWD
+read PASSWD
 echo -n "Enter Windows machine IP and press [ENTER]: "
-read -n 2 IP
+read IP
 sed -i "s/<node_ip>/$IP/g" hosts
 sed -i "s/<password>/\x27$PASSWD\x27/g" hosts
 sed -i "s/<username>/$NAME/g" hosts
