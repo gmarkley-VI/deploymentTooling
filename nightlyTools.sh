ad1d665f12edd36dff25683def8a0c2150ba01a2
gmarkley-VI <gmarkley@redhat.com>
2020-04-09 10:25:32 -0400

    Added error checking and output to highlight what was done.

--- nightlyTools.sh
+++ nightlyTools.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -i
 
 if [ -n "$1" ]; then
-  if [ -f tools ]; then
-    rm -rf tools
-    NEW=false
-  else
-    NEW=true
-  fi
-  mkdir tools
   cd tools || exit
+  rm -rf *
   wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-install-linux.tar.gz
   wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-client-linux.tar.gz
   tar -xzvf openshift-install-linux.tar.gz
   tar -xzvf openshift-client-linux.tar.gz
-  if $NEW; then
-    PREWD="$(pwd)"
-      sudo ln -s $PREWD/oc /usr/local/bin/
-      sudo ln -s $PREWD/openshift-install /usr/local/bin/
-      sudo ln -s $PREWD/kubectl /usr/local/bin/
+  PREWD="$(pwd)"
+  if [ ! -f /usr/local/bin/oc ]; then
+    sudo ln -s $PREWD/oc /usr/local/bin/
+    echo "Setup: sudo ln -s $PREWD/oc /usr/local/bin/"
+  fi
+  if [ ! -f /usr/local/bin/openshift-install ]; then
+    sudo ln -s $PREWD/openshift-install /usr/local/bin/
+    echo "Setup: sudo ln -s $PREWD/openshift-install /usr/local/bin/"
+  fi
+  if [ ! -f /usr/local/bin/kubectl ]; then
+    sudo ln -s $PREWD/kubectl /usr/local/bin/
+    echo "Setup: sudo ln -s $PREWD/kubectl /usr/local/bin/"
   fi
   cd ..
+  echo "oc, kubectl, openshift-install - nightly $1 installed and configured for use."
 else
   echo "Nightly  parameter not supplied. Please provide a nightly string"
 fi
