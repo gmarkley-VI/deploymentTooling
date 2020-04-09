7929f75114b9d0f9fa88f306c420d7e7aebae9db
gmarkley-VI <gmarkley@redhat.com>
2020-04-09 16:39:05 -0400

    Wow so much updated Who knows if this will work lol

--- installWithOVNHybrid.sh
+++ installWithOVNHybrid.sh
@@ -2,6 +2,7 @@
 
 #create the config
 if [ -n "$1" ]; then
+  $1="$(echo $1 | tr -d \/)"
   if [ ! -f $1/install-config.yaml ]; then
     PREWD="$(pwd)"
     openshift-install create install-config --dir=$1
@@ -15,13 +16,43 @@ if [ -n "$1" ]; then
     echo "Install is completed"
     export KUBECONFIG=$PREWD/$1/auth/kubeconfig
     oc get nodes
-    oc get network.operator cluster -o yaml
-    #./wni azure create --kubeconfig $1/auth/kubeconfig --credentials ~/.azure/osServicePrincipal.json --image-id MicrosoftWindowsServer:WindowsServer:2019-Datacenter-with-Containers:latest --instance-type Standard_D2s_v3 --dir $1
-    echo "Create windows worker and RDP into Windows node and setup ansible"
+    HYBRID="$(oc get network.operator cluster -o yaml | grep -cim1 hybridClusterNetwork)"
+    if [ "$HYBRID" = 0 ]; then
+      echo "Hybrid network did not setup correctly"
+      exit
+    else
+      echo "Hybrid network is setup."
+    fi
+
+    #Store the InfraID for future use
+    INFRAID="$(jq -r .infraID $1/metadata.json)"
+
+    MYIP="$(curl ifconfig.me)"
+    echo "Setup firewall Rules for network and for $MYIP to use ansivle"
+    az network nsg rule create -g $INFRAID-rg --nsg-name $INFRAID-node-nsg -n WinRMHTTPS --priority 510 --source-address-prefixes 40.122.148.16 --destination-port-ranges 5986
+    az network nsg rule create -g $INFRAID-rg --nsg-name $INFRAID-node-nsg -n AllLocalWorker --priority 520 --source-address-prefixes 10.0.0.0/16 --destination-port-ranges 0-65535
     #TODO Automate the process of Deployment via a templete here
-    #TODO Automate the firewall rull change for Deployment
+    NODENAME="winnode"$(date +%d%H%M%S)""
+    PASSWD="$(openssl rand -base64 20)"
+    az group create--resource-group $INFRAID-rg
+    az deployment group create \
+      --name ExampleDeployment \
+      --resource-group ExampleGroup \
+      --template-file storage.json \
+      --parameters storageAccountType=Standard_GRS
     #TODO Run Powershell commands here to enable Ansible
+    #NODEIP="$(az vm list-ip-addresses -g gmarkley-fnmpq-rg -n $NODENAME | jq -r '.[] | .virtualMachine.network.publicIpAddresses | .[] | .ipAddress')"
+    #create the hosts file need a config file for this.
+    #az vm run-command invoke --command-id RunPowerShellScript --name $NODENAME -g $INFRAID-rg --scripts backups/ansibleSetupPS
+    #az vm run-command invoke --command-id RunPowerShellScript --name $NODENAME -g $INFRAID-rg --scripts backups/loggingSetupPS
+
+    echo "Setup WMCB"
+    #git clone https://github.com/openshift/windows-machine-config-bootstrapper.git
+    #sed -i '328c\      shell: "echo $NODENAME"' windows-machine-config-bootstrapper/tools/ansible/tasks/wsu/main.yaml
+
     echo "Create hosts file"
+    #TODO combine createHostsFile.sh here
+    #Ansible Commands here down
     echo "Ansible boot strap the windows node"
     echo "ansible win -i hosts -m win_ping -v"
     echo "ansible-playbook -i hosts windows-machine-config-bootstrapper/tools/ansible/tasks/wsu/main.yaml -v"
