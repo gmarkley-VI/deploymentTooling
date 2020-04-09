b00140d7310fd433cd6c64d0d6e4b4ad0c775b60
gmarkley-VI <gmarkley@redhat.com>
2020-04-09 10:06:23 -0400

    Upgraded nightlyTools.sh to include a first run option.

--- nightlyTools.sh
+++ nightlyTools.sh
@@ -3,24 +3,23 @@
 if [ -n "$1" ]; then
   if [ -f tools ]; then
     rm -rf tools
-    mkdir tools
-    cd tools || exit
-    wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-install-linux.tar.gz
-    wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-client-linux.tar.gz
-    tar -xzvf openshift-install-linux.tar.gz
-    tar -xzvf openshift-client-linux.tar.gz
-    cd ..
+    NEW=false
   else
-    mkdir tools
-    cd tools || exit
-    wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-install-linux.tar.gz
-    wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-client-linux.tar.gz
-    tar -xzvf openshift-install-linux.tar.gz
-    tar -xzvf openshift-client-linux.tar.gz
-    cd ..
-    ##TODO - Setup first time run here
-    #Print out the ln -s for the /lib/bin here if its first time run
+    NEW=true
   fi
+  mkdir tools
+  cd tools || exit
+  wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-install-linux.tar.gz
+  wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp-dev-preview/$1/openshift-client-linux.tar.gz
+  tar -xzvf openshift-install-linux.tar.gz
+  tar -xzvf openshift-client-linux.tar.gz
+  if $NEW; then
+    PREWD="$(pwd)"
+      sudo ln -s $PREWD/oc /usr/local/bin/
+      sudo ln -s $PREWD/openshift-install /usr/local/bin/
+      sudo ln -s $PREWD/kubectl /usr/local/bin/
+  fi
+  cd ..
 else
   echo "Nightly  parameter not supplied. Please provide a nightly string"
 fi
