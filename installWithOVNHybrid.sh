ad8119750812a61da3e32e2c44cdcf79b66be762
gmarkley-VI <gmarkley@redhat.com>
2020-04-09 20:02:01 -0400

    Trying to fix the DIR variable.

--- installWithOVNHybrid.sh
+++ installWithOVNHybrid.sh
@@ -2,19 +2,19 @@
 
 #create the config
 if [ -n "$1" ]; then
-  1="$(echo $1 | tr -d \/)"
-  if [ ! -f $1/install-config.yaml ]; then
+  DIR="$(echo $1 | tr -d \/)"
+  if [ ! -f $DIR/install-config.yaml ]; then
     PREWD="$(pwd)"
-    openshift-install create install-config --dir=$1
-    sed -i 's/OpenShiftSDN/OVNKubernetes/g' $1/install-config.yaml
-    openshift-install create manifests --dir=$1
-    patch $1/manifests/cloud-provider-config.yaml backups/cloud-provider-config.patch
-    cp $1/manifests/cluster-network-02-config.yml $1/manifests/cluster-network-03-config.yml
-    patch $1/manifests/cluster-network-03-config.yml backups/cluster-network-03-config.patch
+    openshift-install create install-config --dir=$DIR
+    sed -i 's/OpenShiftSDN/OVNKubernetes/g' $DIR/install-config.yaml
+    openshift-install create manifests --dir=$DIR
+    patch $DIR/manifests/cloud-provider-config.yaml backups/cloud-provider-config.patch
+    cp $DIR/manifests/cluster-network-02-config.yml $DIR/manifests/cluster-network-03-config.yml
+    patch $DIR/manifests/cluster-network-03-config.yml backups/cluster-network-03-config.patch
     echo "Patching Complete Preparing to install"
-    openshift-install create cluster --dir=$1
+    openshift-install create cluster --dir=$DIR
     echo "Install is completed"
-    export KUBECONFIG=$PREWD/$1/auth/kubeconfig
+    export KUBECONFIG=$PREWD/$DIR/auth/kubeconfig
     oc get nodes
     HYBRID="$(oc get network.operator cluster -o yaml | grep -cim1 hybridClusterNetwork)"
     if [ "$HYBRID" = 0 ]; then
