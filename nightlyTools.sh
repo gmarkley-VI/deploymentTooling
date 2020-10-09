6f5779fc899737b940d9bc10e395dab8dbd17d5d
gmarkley-VI <gmarkley@redhat.com>
2020-10-09 09:49:57 -0400

    Remove Symbolic links

--- nightlyTools.sh
+++ nightlyTools.sh
@@ -7,21 +7,6 @@ if [ -n "$1" ]; then
   wget $1/openshift-client-linux.tar.gz
   tar -xzvf openshift-install-linux.tar.gz
   tar -xzvf openshift-client-linux.tar.gz
-  PREWD="$(pwd)"
-  if [ ! -f /usr/local/bin/oc ]; then
-    sudo ln -s $PREWD/oc /usr/local/bin/
-    echo "Setup: sudo ln -s $PREWD/oc /usr/local/bin/"
-  fi
-  if [ ! -f /usr/local/bin/openshift-install ]; then
-    sudo ln -s $PREWD/openshift-install /usr/local/bin/
-    echo "Setup: sudo ln -s $PREWD/openshift-install /usr/local/bin/"
-  fi
-  if [ ! -f /usr/local/bin/kubectl ]; then
-    sudo ln -s $PREWD/kubectl /usr/local/bin/
-    echo "Setup: sudo ln -s $PREWD/kubectl /usr/local/bin/"
-  fi
-  cd ..
-  echo "oc, kubectl, openshift-install - nightly $1 installed and configured for use."
 else
   echo "Download URL parameter not supplied. Please provide a URL to installer and client directory. Example: https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.4.0/"
 fi
