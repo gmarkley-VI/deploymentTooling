9c1ed09cccb95dbc672b5224ce8bb5a825e0d70e
gmarkley-VI <gmarkley@redhat.com>
2020-10-11 17:21:18 -0400

    fix for MAC

--- backups/loggingSetup.ps1
+++
@@ -1,1 +0,0 @@
-New-NetFirewallRule -DisplayName "Enable console logs" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 10250 -EdgeTraversalPolicy Allow
--- installOSOVNHybrid.sh
+++ installOSOVNHybrid.sh
@@ -3,18 +3,17 @@
 #create the config
 if [ -n "$1" ]; then
   if [ ! -f $1/install-config.yaml ]; then
-    openshift-install create install-config --dir=$1
+    tools/openshift-install create install-config --dir=$1
     sed -i.bak "s/OpenShiftSDN/OVNKubernetes/g" $1/install-config.yaml
-    openshift-install create manifests --dir=$1
-    ##patch $1/manifests/cloud-provider-config.yaml backups/cloud-provider-config.patch
+    tools/openshift-install create manifests --dir=$1
     cp $1/manifests/cluster-network-02-config.yml $1/manifests/cluster-network-03-config.yml
     patch $1/manifests/cluster-network-03-config.yml backups/cluster-network-03-config.patch
     echo "Patching Complete Preparing to install"
-    openshift-install create cluster --dir=$1
+    tools/openshift-install create cluster --dir=$1
     echo "Install is completed"
     export KUBECONFIG=$PWD/$1/auth/kubeconfig
-    oc get nodes
-    HYBRID="$(oc get network.operator cluster -o yaml | grep -cim1 hybridClusterNetwork)"
+    tools/oc get nodes
+    HYBRID="$(tools/oc get network.operator cluster -o yaml | grep -cim1 hybridClusterNetwork)"
     if [ "$HYBRID" = 0 ]; then
       echo "Hybrid network did not setup correctly"
       exit
