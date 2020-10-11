a3da13f3c4cc7579e857c2d13b7e3cb3d518d6c3
gmarkley-VI <gmarkley@redhat.com>
2020-10-11 15:46:21 -0400

    Did not need to declare files after update to tooling. Just made things much worse to use.

--- installOSOVNHybrid.sh
+++ installOSOVNHybrid.sh
@@ -5,17 +5,17 @@ if [ -n "$1" ]; then
   DIR="$(echo $1 | tr -d \/)"
   if [ ! -f $DIR/install-config.yaml ]; then
     PREWD="$(pwd)"
-    tools/openshift-install create install-config --dir=$DIR
+    openshift-install create install-config --dir=$DIR
     sed -i 's/OpenShiftSDN/OVNKubernetes/g' $DIR/install-config.yaml
-    tools/openshift-install create manifests --dir=$DIR
+    openshift-install create manifests --dir=$DIR
     patch $DIR/manifests/cloud-provider-config.yaml backups/cloud-provider-config.patch
     cp $DIR/manifests/cluster-network-02-config.yml $DIR/manifests/cluster-network-03-config.yml
     patch $DIR/manifests/cluster-network-03-config.yml backups/cluster-network-03-config.patch
     echo "Patching Complete Preparing to install"
-    tools/openshift-install create cluster --dir=$DIR
+    openshift-install create cluster --dir=$DIR
     echo "Install is completed"
     export KUBECONFIG=$PREWD/$DIR/auth/kubeconfig
-    tools/oc get nodes
+    oc get nodes
     HYBRID="$(tools/oc get network.operator cluster -o yaml | grep -cim1 hybridClusterNetwork)"
     if [ "$HYBRID" = 0 ]; then
       echo "Hybrid network did not setup correctly"
